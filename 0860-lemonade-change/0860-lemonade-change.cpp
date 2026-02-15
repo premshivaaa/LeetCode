e4135aca56f20a73516712954ae3a44e21afa0bf
Premshiva Chanumuri <chanumuripremshiva@gmail.com>
2026-02-15 15:28:35 +0530

    Time: 0 ms (100%), Space: 87.2 MB (75.51%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fives = 0, tens = 0, twenties = 0;
+
+        for(int i : bills){
+            if(i == 5){
+                fives++;
+            }
+            else if(i == 10){
+                if(fives >= 1){
+                    tens++;
+                    fives--;
+                } else{
+                    return false;
+                }
+            }
+            else{
+                if(tens >=1 && fives>=1){
+                    tens--;
+                    fives--;
+                } else if(fives >= 3) {
+                    fives -= 3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
