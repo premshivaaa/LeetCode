4a1a78f04e26af5a7ca7ededd2a63037257cac5b
Premshiva Chanumuri <chanumuripremshiva@gmail.com>
2026-02-16 16:40:47 +0530

    Time: 0 ms (100%), Space: 22.9 MB (53.25%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        for(int i=1; i<n; i++){
+            if(ratings[i-1] < ratings[i]){
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i+1] < ratings[i]){
+                int c = candies[i+1] + 1;
+                candies[i] = max(candies[i], c);
+            }
+        }
+        int candy = 0;
+
+        for(int i : candies){
+            candy += i;
+        }
+
+        return candy;
+    }
+};
