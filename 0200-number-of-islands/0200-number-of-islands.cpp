95f44ec555cbb3b50892c198d25e6a1686a04e5f
Premshiva Chanumuri <chanumuripremshiva@gmail.com>
2026-03-05 19:18:59 +0530

    Time: 21 ms (91.87%), Space: 24.2 MB (19.04%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,8 +1,8 @@
 class Solution {
 
 private:
-    void traverse(int row, int col, vector<vector<char>>& grid, int m, int n){
-        grid[row][col] = '0';
+    void traverse(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid, int m, int n){
+        vis[row][col] = 1;
         queue<pair<int,int>> q;
         q.push({row,col});
 
@@ -19,8 +19,8 @@ private:
                 int nrow = delrow[i] + row;
                 int ncol = delcol[i] + col;
 
-                if(nrow < m && nrow >= 0 && ncol < n && ncol >= 0 && grid[nrow][ncol] == '1'){
-                    grid[nrow][ncol] = '0';
+                if(nrow < m && nrow >= 0 && ncol < n && ncol >= 0 && !vis[nrow][ncol] && grid[nrow][ncol] == '1'){
+                    vis[nrow][ncol] = 1;
                     q.push({nrow, ncol});
                 } 
             }
@@ -33,13 +33,13 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        // vector<vector<int>> vis(m , vector<int>(n, 0));
+        vector<vector<int>> vis(m , vector<int>(n, 0));
         int count = 0;
 
         for(int row = 0; row < m; row++){
             for(int col = 0; col < n; col++){
-                if(grid[row][col] == '1'){
-                    traverse(row, col, grid, m, n);
+                if(!vis[row][col] && grid[row][col] == '1'){
+                    traverse(row, col, vis, grid, m, n);
                     count++;
                 }
             }
