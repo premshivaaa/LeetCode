2f74207a535740850e390cdd0b3a6d804d8daf97
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-03-12 16:18:53 +0530

    Time: 7 ms (74.3%), Space: 38.1 MB (18.11%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,10 +1,10 @@
 class Solution {
     int delrow[4] = {-1, 0, 1, 0};
     int delcol[4] = {0, -1, 0, 1};
-    //int m, n;
+    int m, n;
 
 private:
-    void dfs(int row, int col, vector<vector<int>>& vis, vector<vector<int>>& grid, int m , int n){
+    void dfs(int row, int col, vector<vector<int>>& vis, vector<vector<int>>& grid){
         vis[row][col] = 1;
 
         for(int i=0; i<4; i++){
@@ -12,27 +12,27 @@ private:
             int ncol = delcol[i] + col;
 
             if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
-                dfs(nrow, ncol, vis, grid, m, n);
+                dfs(nrow, ncol, vis, grid);
             }
         }
     }
 
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        m = grid.size();
+        n = grid[0].size();
         vector<vector<int>> vis(m, vector<int>(n, 0));
 
         for(int j=0; j<n; j++){
-            if(!vis[0][j] && grid[0][j]) dfs(0, j, vis, grid, m, n);
+            if(!vis[0][j] && grid[0][j]) dfs(0, j, vis, grid);
                 
-            if(!vis[m-1][j] && grid[m-1][j]) dfs(m-1, j, vis, grid, m, n);
+            if(!vis[m-1][j] && grid[m-1][j]) dfs(m-1, j, vis, grid);
                 
         }
         for(int i=0; i<m; i++){
-            if(!vis[i][0] && grid[i][0]) dfs(i, 0, vis, grid, m, n);
+            if(!vis[i][0] && grid[i][0]) dfs(i, 0, vis, grid);
                 
-            if(!vis[i][n-1] && grid[i][n-1]) dfs(i, n-1, vis, grid, m, n);
+            if(!vis[i][n-1] && grid[i][n-1]) dfs(i, n-1, vis, grid);
                 
         }
 
