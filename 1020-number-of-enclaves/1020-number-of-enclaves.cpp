e1cdc2d9f4ea7a0327bf4685006db33924f87cb0
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-03-12 16:23:34 +0530

    Time: 8 ms (61.96%), Space: 34 MB (54.15%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -4,15 +4,15 @@ class Solution {
     int m, n;
 
 private:
-    void dfs(int row, int col, vector<vector<int>>& vis, vector<vector<int>>& grid){
-        vis[row][col] = 1;
+    void dfs(int row, int col, vector<vector<int>>& grid){
+        grid[row][col] = 0;
 
         for(int i=0; i<4; i++){
             int nrow = delrow[i] + row;
             int ncol = delcol[i] + col;
 
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
-                dfs(nrow, ncol, vis, grid);
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol] == 1){
+                dfs(nrow, ncol, grid);
             }
         }
     }
@@ -21,25 +21,25 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int>(n, 0));
+        //vector<vector<int>> vis(m, vector<int>(n, 0));
 
         for(int j=0; j<n; j++){
-            if(!vis[0][j] && grid[0][j]) dfs(0, j, vis, grid);
+            if(grid[0][j]) dfs(0, j, grid);
                 
-            if(!vis[m-1][j] && grid[m-1][j]) dfs(m-1, j, vis, grid);
+            if(grid[m-1][j]) dfs(m-1, j, grid);
                 
         }
         for(int i=0; i<m; i++){
-            if(!vis[i][0] && grid[i][0]) dfs(i, 0, vis, grid);
+            if(grid[i][0]) dfs(i, 0, grid);
                 
-            if(!vis[i][n-1] && grid[i][n-1]) dfs(i, n-1, vis, grid);
+            if(grid[i][n-1]) dfs(i, n-1, grid);
                 
         }
 
         int lands = 0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(!vis[i][j] && grid[i][j] == 1){
+                if(grid[i][j] == 1){
                     lands++;
                 }
             }
