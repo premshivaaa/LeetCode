35e922231fda9bc7d95403c84207c34eff414e2b
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-03-18 16:50:00 +0530

    Time: 8 ms (36.2%), Space: 20.2 MB (26.45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,35 @@
 class Solution {
+
+private:
+    bool dfs(int node, vector<vector<int>>& adj, vector<int>& vis){
+        vis[node] = 2;
+
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                if(dfs(it, adj, vis) == false) return false;
+            } else if (vis[it] == 2){
+                return false;
+            }
+        }
+        vis[node] = 1;
+        return true;
+    }
+
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses);
-        int count = 0;
+        vector<int> vis(numCourses, 0);
 
         for(auto it : prerequisites){
-            adj[it[1]].push_back(it[0]);
-            indegree[it[0]]++;
+            adj[it[0]].push_back(it[1]);
         }
-        queue<int> q;
+
         for(int i=0; i<numCourses; i++){
-            if(indegree[i] == 0) q.push(i);
-        }
-
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            count++;
-
-            for(auto it : adj[node]){
-                indegree[it]--;
-                if(indegree[it] == 0) q.push(it);
+            if(!vis[i]){
+                if(dfs(i, adj, vis) == false) return false;
             }
         }
-        if(numCourses == count) return true;
 
-        return false;
+        return true;
     }
 };
