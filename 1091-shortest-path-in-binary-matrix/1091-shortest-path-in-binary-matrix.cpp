032abbdca02afe23f00efb88c9575f1224c0eceb
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-03-30 16:49:57 +0530

    Time: 11 ms (86.72%), Space: 23.3 MB (98.57%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,34 +2,46 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
+        
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
-        vector<vector<int>> dist(n, vector(n, INT_MAX));
-        dist[0][0] = 1;
+
         queue<pair<int,int>> q;
         q.push({0, 0});
+        
+        // mark visited
+        grid[0][0] = 1;
 
         int delrow[8] = {-1,-1,-1,0,0,1,1,1};
         int delcol[8] = {-1,0,1,-1,1,-1,0,1};
 
+        int steps = 1;
+
         while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
+            int size = q.size();
+            
+            while(size--){
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
 
-            for(int i=0; i<8; i++){
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
+                // Early exit
+                if(row == n-1 && col == n-1) return steps;
 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && 
-                dist[row][col] + 1 < dist[nrow][ncol] && grid[nrow][ncol] == 0){
-                    dist[nrow][ncol] = dist[row][col] + 1;
-                    q.push({nrow, ncol});
+                for(int i=0; i<8; i++){
+                    int nrow = row + delrow[i];
+                    int ncol = col + delcol[i];
+
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<n 
+                       && grid[nrow][ncol] == 0){
+                        
+                        q.push({nrow, ncol});
+                        grid[nrow][ncol] = 1; // mark visited
+                    }
                 }
             }
+            steps++;
         }
 
-        if(dist[n-1][n-1] == INT_MAX) return -1;
-
-        return dist[n-1][n-1];
+        return -1;
     }
 };
