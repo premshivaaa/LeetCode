15a463b6462fe3ed02d5c15fe91cfef422442a53
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-04-06 12:41:41 +0530

    Time: 3 ms (86.19%), Space: 41.6 MB (99.51%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,72 +1,33 @@
-class DisjointSet{   
-public:
-vector<int> rank, parent, size;
-    DisjointSet(int n){
-        rank.resize(n+1, 0);
-        parent.resize(n+1);
-        size.resize(n+1, 1);
-        for(int i=0; i<=n; i++) parent[i] = i;
-    }
-    int findUParent(int node){
-        if(parent[node] == node) return node;
-        return parent[node] = findUParent(parent[node]);
-    }
-    void unionByRank(int u, int v){
-        int ult_u = findUParent(u);
-        int ult_v = findUParent(v);
-        if(ult_u == ult_v) return;
-        
-        if(rank[ult_u] < rank[ult_v]){
-            parent[ult_u] = ult_v;
-        }
-        else if(rank[ult_v] < rank[ult_u]){
-            parent[ult_v] = ult_u;
-        }
-        else{
-            parent[ult_v] = ult_u;
-            rank[ult_u]++;
-        }
-    }
-    void unionBySize(int u, int v){
-        int ult_u = findUParent(u);
-        int ult_v = findUParent(v);
-        if(ult_u == ult_v) return;
-        
-        if(size[ult_u] < size[ult_v]){
-            parent[ult_u] = ult_v;
-            size[ult_v] += size[ult_u];
-        }
-        else{
-            parent[ult_v] = ult_u;
-            size[ult_u] += size[ult_v];
-        }
-    }
-};
-
 class Solution {
 public:
+    int findparent(int node, vector<int>& parent) {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = findparent(parent[node], parent);
+    }
+
+
     int makeConnected(int n, vector<vector<int>>& connections) {
-        DisjointSet ds(n);
-        int extra = 0;
-        for(auto edge : connections){
+        if (connections.size() < n - 1)
+            return -1;
+        // declaration 
+        vector<int> parent(n);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        int comp = n;
+        for (const auto& edge : connections) {
             int u = edge[0];
             int v = edge[1];
-
-            if(ds.findUParent(u) != ds.findUParent(v)){
-                ds.unionBySize(u, v);
-            }
-            else {
-                extra++;
+            int parentU = findparent(u, parent);
+            int parentV = findparent(v, parent);
+            
+            if (parentU != parentV) {
+                parent[parentU] = parentV;
+                comp--;
             }
         }
-        int components = 0;
-        for(int i=0; i<n; i++){
-            if(ds.parent[i] == i) components++;
-        }
-        
-        if(extra >= components-1) return components-1;
-
-        return -1;
-
+        return comp - 1;
     }
 };
