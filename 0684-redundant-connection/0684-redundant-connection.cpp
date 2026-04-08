586298a4416795a7692119f4951d5e992d2a8ec4
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-04-08 14:31:00 +0530

    Time: 0 ms (100%), Space: 13.4 MB (39.44%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,74 @@
+class DisjointSet{
+    public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n){
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        size.resize(n+1, 1);
+        for(int i=0; i<=n; i++) parent[i] = i;
+    }
+    int findUParent(int node){
+        if(parent[node] == node) return node;
+        return parent[node] = findUParent(parent[node]);
+    }
+    void unionByRank(int u, int v){
+        int ult_u = findUParent(u);
+        int ult_v = findUParent(v);
+        if(ult_u == ult_v) return;
+        
+        if(rank[ult_u] < rank[ult_v]){
+            parent[ult_u] = ult_v;
+        }
+        else if(rank[ult_v] < rank[ult_u]){
+            parent[ult_v] = ult_u;
+        }
+        else{
+            parent[ult_v] = ult_u;
+            rank[ult_u]++;
+        }
+    }
+    void unionBySize(int u, int v){
+        int ult_u = findUParent(u);
+        int ult_v = findUParent(v);
+        if(ult_u == ult_v) return;
+        
+        if(size[ult_u] < size[ult_v]){
+            parent[ult_u] = ult_v;
+            size[ult_v] += size[ult_u];
+        }
+        else{
+            parent[ult_v] = ult_u;
+            size[ult_u] += size[ult_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        DisjointSet ds(n);
+        vector<int> ans;
+        for(auto edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            if(ds.findUParent(u) != ds.findUParent(v)){
+                ds.unionBySize(u, v);
+            }
+            else{
+                if(ans.size() == 0){
+                    ans.push_back(u);
+                    ans.push_back(v);
+                }
+                else{
+                    ans.pop_back();
+                    ans.pop_back();
+                    ans.push_back(u);
+                    ans.push_back(v);
+                }
+            }
+        }
+        return ans;
+    }
+};
