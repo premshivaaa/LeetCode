5c646ba2345eee68fb6dcf4bdbf0da0677b16776
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-04-15 00:14:31 +0530

    Time: 21 ms (53.24%), Space: 24.8 MB (23.69%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,8 +1,7 @@
 class DisjointSet{
     public:
-    vector<int> rank, parent, size;
+    vector<int> parent, size;
     DisjointSet(int n){
-        rank.resize(n+1, 0);
         parent.resize(n+1);
         size.resize(n+1, 1);
         for(int i=0; i<=n; i++) parent[i] = i;
@@ -10,22 +9,6 @@ class DisjointSet{
     int findUParent(int node){
         if(parent[node] == node) return node;
         return parent[node] = findUParent(parent[node]);
-    }
-    void unionByRank(int u, int v){
-        int ult_u = findUParent(u);
-        int ult_v = findUParent(v);
-        if(ult_u == ult_v) return;
-        
-        if(rank[ult_u] < rank[ult_v]){
-            parent[ult_u] = ult_v;
-        }
-        else if(rank[ult_v] < rank[ult_u]){
-            parent[ult_v] = ult_u;
-        }
-        else{
-            parent[ult_v] = ult_u;
-            rank[ult_u]++;
-        }
     }
     void unionBySize(int u, int v){
         int ult_u = findUParent(u);
