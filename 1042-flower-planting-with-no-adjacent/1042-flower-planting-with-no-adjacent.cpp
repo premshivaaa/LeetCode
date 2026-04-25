1649cc09c5f01f02f5a07b454214a2f382c13ab5
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-04-25 12:02:35 +0530

    Time: 27 ms (91.25%), Space: 45 MB (78.6%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -11,7 +11,7 @@ public:
         vector<int> ans(n + 1, 0);
 
         for (int i = 1; i <= n; i++) {
-            vector<bool> used(5, false);
+            bool used[5] = {false};
 
             for (auto it : adj[i]) {
                 used[ans[it]] = true;
