86bb9409b007d4b7d4fd8cc0598cd7ef2048b297
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-05-23 11:53:27 +0530

    Time: 7 ms (82.94%), Space: 224 MB (80.2%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,73 +1,19 @@
-class DisjointSet{
-    public:
-    vector<int> rank, parent, size;
-    DisjointSet(int n){
-        rank.resize(n+1, 0);
-        parent.resize(n+1);
-        size.resize(n+1, 1);
-        for(int i=0; i<=n; i++) parent[i] = i;
-    }
-    int findUParent(int node){
-        if(parent[node] == node) return node;
-        return parent[node] = findUParent(parent[node]);
-    }
-    void unionByRank(int u, int v){
-        int ult_u = findUParent(u);
-        int ult_v = findUParent(v);
-        if(ult_u == ult_v) return;
-        
-        if(rank[ult_u] < rank[ult_v]){
-            parent[ult_u] = ult_v;
-        }
-        else if(rank[ult_v] < rank[ult_u]){
-            parent[ult_v] = ult_u;
-        }
-        else{
-            parent[ult_v] = ult_u;
-            rank[ult_u]++;
-        }
-    }
-    void unionBySize(int u, int v){
-        int ult_u = findUParent(u);
-        int ult_v = findUParent(v);
-        if(ult_u == ult_v) return;
-        
-        if(size[ult_u] < size[ult_v]){
-            parent[ult_u] = ult_v;
-            size[ult_v] += size[ult_u];
-        }
-        else{
-            parent[ult_v] = ult_u;
-            size[ult_u] += size[ult_v];
-        }
-    }
-};
-
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        // if (maxDiff == 0) {
-        //     return vector<bool>(queries.size(), true);
-        // }
+        vector<int> group(n, 0);
 
-        vector<bool> ans;
-        DisjointSet ds(n);
-
-        for(int i=0; i<nums.size()-1; i++){
-            if(abs(nums[i] - nums[i+1]) <= maxDiff){
-                ds.unionBySize(i, i+1);
-            }
+        for (int i = 1; i < n; i++) {
+            if (abs(nums[i] - nums[i - 1]) <= maxDiff)
+                group[i] = group[i - 1];
+            else
+                group[i] = i;
         }
 
-        for(auto &que : queries){
-            int u = que[0];
-            int v = que[1];
+        vector<bool> ans;
 
-            if(u == v) ans.push_back(true);
-            else{
-                if(ds.findUParent(u) == ds.findUParent(v)) ans.push_back(true);
-                else ans.push_back(false);
-            }
+        for (auto &q : queries) {
+            ans.push_back(group[q[0]] == group[q[1]]);
         }
 
         return ans;
