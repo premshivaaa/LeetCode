67d4faf7586b893be903b5318fd4a74ee0a30496
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-07-10 20:32:26 +0530

    Time: 0 ms (100%), Space: 9 MB (9.36%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,14 +1,15 @@
 class Solution {
 private:
-    bool recursion(int n, long long value){
-        if(n == value) return true;
-        if(value > n) return false;
+    bool recursion(int n, long long value) {
+        if (value == n) return true;
+        if (value > n) return false;
 
-        return recursion(n, value*3);
+        return recursion(n, value * 3);
     }
+
 public:
     bool isPowerOfThree(int n) {
-        long long value = 1;
-        return recursion(n, value);
+        if (n <= 0) return false;
+        return recursion(n, 1);
     }
 };
