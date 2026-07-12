82fe2add33ad1eb9f8d59a565da6be51331578a7
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-07-12 18:51:37 +0530

    Time: 0 ms (100%), Space: 9.1 MB (65.39%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,22 +1,11 @@
 class Solution {
-    int n, total_val;
-private:
-    void recursion(int idx, vector<int>& nums, int xr_val){
-        if(idx == n){
-            total_val += xr_val;
-            return;
-        }
-
-        recursion(idx+1, nums, xr_val ^ nums[idx]);
-
-        recursion(idx+1, nums, xr_val);
-    }
 public:
     int subsetXORSum(vector<int>& nums) {
-        n = nums.size();
-        total_val = 0;
-        recursion(0, nums, 0);
+        int OR = 0;
 
-        return total_val;
+        for (int x : nums)
+            OR |= x;
+
+        return OR * (1 << (nums.size() - 1));
     }
 };
