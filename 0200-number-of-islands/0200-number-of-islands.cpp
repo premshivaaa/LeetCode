00d7b4f3756de87a62da6e4fe1e5a43eca581275
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-07-14 19:45:49 +0530

    Time: 28 ms (49.31%), Space: 17.6 MB (43.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,45 +1,32 @@
 class Solution {
-
+    int m,n;
+    int delrow[4] = {0, 1, 0, -1};
+    int delcol[4] = {-1, 0, 1, 0};
 private:
-    void traverse(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid, int m, int n){
+    void dfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>& vis){
         vis[row][col] = 1;
-        queue<pair<int,int>> q;
-        q.push({row,col});
 
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, 1, 0, -1};
+        for(int i=0; i<4; i++){
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
 
-
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-
-            for(int i = 0; i < 4; i++){
-                int nrow = delrow[i] + row;
-                int ncol = delcol[i] + col;
-
-                if(nrow < m && nrow >= 0 && ncol < n && ncol >= 0 && !vis[nrow][ncol] && grid[nrow][ncol] == '1'){
-                    vis[nrow][ncol] = 1;
-                    q.push({nrow, ncol});
-                } 
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol] && grid[nrow][ncol] == '1'){
+                dfs(nrow, ncol, grid, vis);
             }
         }
-  
+        return;      
     }
-
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        vector<vector<int>> vis(m , vector<int>(n, 0));
+        m = grid.size();
+        n = grid[0].size();
+        vector<vector<int>> vis(m, vector<int>(n, 0));
         int count = 0;
 
-        for(int row = 0; row < m; row++){
-            for(int col = 0; col < n; col++){
-                if(!vis[row][col] && grid[row][col] == '1'){
-                    traverse(row, col, vis, grid, m, n);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(!vis[i][j] && grid[i][j] == '1'){
+                    dfs(i, j, grid, vis);
                     count++;
                 }
             }
