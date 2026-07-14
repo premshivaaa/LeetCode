18f3270fbbd0f1707b83c729b7422b14f18bbd26
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-07-14 19:17:22 +0530

    Time: 0 ms (100%), Space: 16.6 MB (86.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,57 +3,44 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        queue<pair<pair<int, int> , int>> q;
-        //int vis[m][n];
-        int fresh = 0;
-        int cnt = 0;
+        //vector<vector<int>> vis(m, vector<int>(n, 0));
+        queue<pair<int, pair<int,int>>> q;
+        int total_time = 0;
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == 2){
-                   //vis[i][j] = 2;
-                    q.push({{i, j}, 0});
-                }
-                if(grid[i][j] == 1){
-                    fresh++;
+                    //vis[i][j] = 1;
+                    q.push({0, {i, j}});
                 }
             }
         }
-
-        int time = 0;
-        int delrow[4] = {-1, 1, 0, 0};
-        int delcol[4] = {0, 0, -1, 1};
-
+        int delrow[4] = {0, 1, 0, -1};
+        int delcol[4] = {1, 0, -1, 0};
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int tm  = q.front().second;
-            time = max(time, tm);
+            int time = q.front().first;
+            int row = q.front().second.first;
+            int col = q.front().second.second;
             q.pop();
+
+            total_time = time;
 
             for(int i=0; i<4; i++){
                 int nrow = row + delrow[i];
                 int ncol = col + delcol[i];
 
                 if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol] == 1){
-                    q.push({{nrow, ncol}, tm + 1});
+                    //vis[nrow][ncol] = 1;
                     grid[nrow][ncol] = 2;
-                    cnt++;
+                    q.push({time+1, {nrow, ncol}});
                 }
             }
         }
-
-        // for(int i=0; i<m; i++){
-        //     for(int j=0; j<n; j++){
-        //         if(vis[i][j] != 2 && grid[i][j] == 1){
-        //             return -1;
-        //         }
-        //     }
-        // }
-        if(fresh != cnt){
-            return -1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1) return -1;
+            }
         }
-
-        return time;
+        return total_time;
     }
 };
