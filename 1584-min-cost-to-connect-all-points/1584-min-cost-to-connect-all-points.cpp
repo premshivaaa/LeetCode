c2b0004815fb9ea112365e2571cf5ed3de554e15
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-07-24 19:53:02 +0530

    Time: 23 ms (91.25%), Space: 14.6 MB (90.55%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,83 +1,30 @@
-class DSU {
-public:
-    vector<int> parent, rank;
-
-    DSU(int n) {
-        parent.resize(n);
-        rank.resize(n, 0);
-
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
-    }
-
-    int findParent(int node) {
-        if (parent[node] == node)
-            return node;
-
-        return parent[node] = findParent(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        u = findParent(u);
-        v = findParent(v);
-
-        if (u == v) return;
-
-        if (rank[u] < rank[v]) {
-            parent[u] = v;
-        }
-        else if (rank[v] < rank[u]) {
-            parent[v] = u;
-        }
-        else {
-            parent[v] = u;
-            rank[u]++;
-        }
-    }
-};
-
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-
         int n = points.size();
+        vector<int> dist(n, INT_MAX);
+        vector<int> vis(n, 0);
+        dist[0] = 0;
+        int ans = 0;
 
-        // {weight, {u, v}}
-        vector<pair<int, pair<int, int>>> edges;
+        for(int i=0; i<n; i++){
+            int minIndex = -1;
+            for(int j=0; j<n; j++){
+                if(!vis[j] && (minIndex == -1 || dist[j] < dist[minIndex])) minIndex = j;
+            }
 
-        // Build all edges
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int wt = abs(points[i][0] - points[j][0]) +
-                         abs(points[i][1] - points[j][1]);
+            vis[minIndex] = 1;
+            ans += dist[minIndex];
 
-                edges.push_back({wt, {i, j}});
+            for(int k=0; k<n; k++){
+                if(!vis[k]){
+                    int cost = abs(points[minIndex][0] - points[k][0]) + abs(points[minIndex][1] - points[k][1]);
+
+                    dist[k] = min(dist[k], cost);
+                }
             }
         }
 
-        sort(edges.begin(), edges.end());
-
-        DSU dsu(n);
-
-        int cost = 0;
-        int edgesUsed = 0;
-
-        for (auto &edge : edges) {
-            int wt = edge.first;
-            int u = edge.second.first;
-            int v = edge.second.second;
-
-            if (dsu.findParent(u) != dsu.findParent(v)) {
-                dsu.unionByRank(u, v);
-                cost += wt;
-                edgesUsed++;
-
-                // MST has exactly (n - 1) edges
-                if (edgesUsed == n - 1)
-                    break;
-            }
-        }
-
-        return cost;
+        return ans;
     }
 };
