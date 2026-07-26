aac7cfb5ac88dbd8724cd60edf3119116298173f
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-07-27 00:37:44 +0530

    Time: 93 ms (70.62%), Space: 107.7 MB (54.39%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,30 +1,39 @@
 class Solution {
 private:
-    void dfs(int node, vector<vector<pair<int,int>>>& adj, vector<int>& vis, int& count){
+    void dfs(int node, vector<vector<pair<int, int>>>& adj,
+             vector<int>& vis, int& ans) {
+
         vis[node] = 1;
 
-        for(auto &it : adj[node]){
-            int edge = it.first;
-            int wt = it.second;
+        for (auto &it : adj[node]) {
+            int next = it.first;
+            int cost = it.second;
 
-            if(!vis[edge]){
-                count += wt;
-                dfs(edge, adj, vis, count);
+            if (!vis[next]) {
+                ans += cost;
+                dfs(next, adj, vis, ans);
             }
         }
     }
+
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
-        vector<vector<pair<int,int>>> adj(n);
-        vector<int> vis(n, 0);
-        for(auto it : connections){
-            adj[it[0]].push_back({it[1], 1});
-            adj[it[1]].push_back({it[0], 0});
+
+        vector<vector<pair<int, int>>> adj(n);
+
+        for (auto &edge : connections) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back({v, 1}); // original direction
+            adj[v].push_back({u, 0}); // reverse direction
         }
 
-        int count = 0;
-        dfs(0, adj, vis, count);
-        
-        return count;
+        vector<int> vis(n, 0);
+        int ans = 0;
+
+        dfs(0, adj, vis, ans);
+
+        return ans;
     }
 };
