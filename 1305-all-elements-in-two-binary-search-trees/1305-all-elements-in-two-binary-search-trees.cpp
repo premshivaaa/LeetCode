e2d23dc0fbbe1ec19f6ac8bca6f363a4a1f79ee4
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-08-17 11:51:39 +0530

    Time: 7 ms (53.75%), Space: 88.7 MB (10.88%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -1,28 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    void traverse(TreeNode* node, vector<int>& array){
-        if(node == NULL) return;
+    void inorder(TreeNode* root, vector<int>& arr) {
+        if (!root) return;
 
-        traverse(node->left, array);
-        array.push_back(node->val);
-        traverse(node->right, array);
+        inorder(root->left, arr);
+        arr.push_back(root->val);
+        inorder(root->right, arr);
     }
+
 public:
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> list;
-        traverse(root1, list);
-        traverse(root2, list);
-        sort(list.begin(), list.end());
-        return list;
+        vector<int> a, b;
+
+        inorder(root1, a);
+        inorder(root2, b);
+
+        vector<int> ans;
+
+        int i = 0, j = 0;
+
+        while (i < a.size() && j < b.size()) {
+            if (a[i] <= b[j])
+                ans.push_back(a[i++]);
+            else
+                ans.push_back(b[j++]);
+        }
+
+        while (i < a.size())
+            ans.push_back(a[i++]);
+
+        while (j < b.size())
+            ans.push_back(b[j++]);
+
+        return ans;
     }
 };
