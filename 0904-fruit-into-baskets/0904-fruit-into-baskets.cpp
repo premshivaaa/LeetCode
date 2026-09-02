187fec599a5bf4e8887f7876e8ba6120587f11a6
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-09-02 09:11:10 +0530

    Time: 60 ms (6.03%), Space: 88.8 MB (7.46%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -8,11 +8,11 @@ public:
             mp[fruits[right]]++;
 
             if(mp.size() > 2){
-                while(mp.size() > 2){
+                // while(mp.size() > 2){
                     mp[fruits[left]]--;
                     if(mp[fruits[left]] == 0) mp.erase(fruits[left]);
                     left++;
-                }
+                // }
             }
             maxlen = max(maxlen, right-left+1);
             right++;
