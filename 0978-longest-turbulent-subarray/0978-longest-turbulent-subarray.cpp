52207bc98cf109164cd63fe27e835789c3716bd9
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-09-14 14:53:44 +0530

    Time: 0 ms (100%), Space: 44.3 MB (15.53%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int maxTurbulenceSize(vector<int>& arr) {
-        int n = arr.size();
-        if (n == 1) return 1;
+        int up = 1, down = 1, ans = 1;
 
-        int left = 0;
-        int ans = 1;
-
-        for (int right = 1; right < n; right++) {
-            int cmp = (arr[right] > arr[right - 1]) ? 1 :
-                      (arr[right] < arr[right - 1]) ? -1 : 0;
-
-            if (cmp == 0) {
-                left = right;
-            } else if (right == n - 1 ||
-                       cmp * ((arr[right + 1] > arr[right]) ? 1 :
-                              (arr[right + 1] < arr[right]) ? -1 : 0) != -1) {
-                ans = max(ans, right - left + 1);
-                left = right;
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] > arr[i - 1]) {
+                up = down + 1;
+                down = 1;
+            } else if (arr[i] < arr[i - 1]) {
+                down = up + 1;
+                up = 1;
+            } else {
+                up = down = 1;
             }
+            ans = max(ans, max(up, down));
         }
 
         return ans;
