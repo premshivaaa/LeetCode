afe71c305d1b8fb120c8a9a904a46fa7e4af60ea
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-09-24 19:47:31 +0530

    Time: 0 ms (100%), Space: 9.9 MB (76.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string modified;
-        for (char c : s) {
-            if (isalnum(c)) {
-                modified += tolower(c);
-            }
-        }
+        int left = 0;
+        int right = s.size() - 1;
 
-        int left = 0, right = modified.size() - 1;
         while (left < right) {
-            if (modified[left] != modified[right])
+
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            if (tolower(s[left]) != tolower(s[right])) {
                 return false;
+            }
+
             left++;
             right--;
         }
+
         return true;
     }
 };
