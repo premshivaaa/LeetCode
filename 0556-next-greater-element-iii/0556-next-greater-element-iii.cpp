e9837446e997de6e95297bae3b4befbf5501322a
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-09-25 23:57:10 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.14%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s = to_string(n);
+
+        int pivot = -1;
+
+        for (int i = s.size() - 2; i >= 0; i--) {
+            if (s[i] < s[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if (pivot == -1)
+            return -1;
+
+        for (int i = s.size() - 1; i > pivot; i--) {
+            if (s[i] > s[pivot]) {
+                swap(s[i], s[pivot]);
+                break;
+            }
+        }
+
+        reverse(s.begin() + pivot + 1, s.end());
+
+        long long ans = stoll(s);
+
+        return (ans > INT_MAX) ? -1 : (int)ans;
+    }
+};
