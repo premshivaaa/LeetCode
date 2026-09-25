eaf226aa8be5f2e83332f01a22b3ef392df84741
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-09-25 14:11:40 +0530

    Time: 0 ms (100%), Space: 10.3 MB (73.77%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,22 +1,32 @@
 class Solution {
 private:
     bool isVowel(char ch) {
-    // Returns true if ch is found in the vowels string
-    return string("aeiouAEIOU").find(ch) != string::npos;
-}
+        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+               ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U';
+    }
+
 public:
     string reverseVowels(string s) {
-        int left = 0, right = s.size()-1;
-        while(left < right){
-            if(!isVowel(s[left])) left++;
-            if(!isVowel(s[right])) right--;
 
-            if(isVowel(s[left]) && isVowel(s[right])){
-                swap(s[left], s[right]);
+        int left = 0;
+        int right = s.size() - 1;
+
+        while(left < right) {
+
+            while(left < right && !isVowel(s[left])) {
                 left++;
+            }
+
+            while(left < right && !isVowel(s[right])) {
                 right--;
-            } 
+            }
+
+            swap(s[left], s[right]);
+
+            left++;
+            right--;
         }
+
         return s;
     }
 };
